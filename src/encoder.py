74b709576e6e13bea20efcdf78367ea60ce64bfc
sameasabove-tech <sameasabove.tech@gmail.com>
2023-10-08 13:15:32 -0400

    This commit will be the summarizer feature. The purpose will be to take a document and summarize the document based on the pretrained model chosen. This is implemented using the HF pipeline module with the summarizer task. The output of the Summary class will be a list of summaries corrsponding to the number of passages the class is given.

--- src/encoder.py
+++ src/encoder.py
@@ -1,5 +1,5 @@
 # look up models that do encoding to a document to vector, src level, encoder.py, have an enocder class 
-
+# https://github.com/UKPLab/sentence-transformers
 # import os
 import numpy as np 
 from sentence_transformers import SentenceTransformer
@@ -9,9 +9,13 @@ from dataclasses import dataclass
 
 @dataclass
 class TextEmbedding:
-    """
-    _summary_
-    
+    """Data class that will hold a document and transform it into a text embedding.
+
+    Attributes:
+        document (list[str]): List of text data inputted into the class. 
+        id (str): Variable unique string of characters initialized when the class is called.
+        model_name (str): LLM of our choice used create the text embedding.
+        text_embedding (None): Text embedding place holder.
     """
     document: list[str] 
     id = uuid1() 
@@ -20,6 +24,8 @@ class TextEmbedding:
 
 
     def encode_document(self): 
+        """Document data will be transformed to a text emedding and stored in the test_embedding variable of the dataclass.
+        """
         model = SentenceTransformer(self.model_name)
         self.text_embedding = model.encode(self.document)
         # return self.text_embedding
